565b8c012790f3c14b68368b8ac118606a177dde
github username <github email address>
2018-07-05 21:43:01 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
     
   namespace :admin do
-    resources :preferences, only: [:index]
+    resources :preferences
   end
     
   resources :artists do
     resources :songs, only: [:index, :show]
   end
+    
   resources :songs
 
 end
